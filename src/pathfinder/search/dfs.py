6d0f41f513357b33a8b46cc4b2786772311beb57
Juan Wagner <juanswagner@gmail.com>
2023-05-03 14:38:27 -0300

    refactor bfs and dfs

--- src/pathfinder/search/dfs.py
+++ src/pathfinder/search/dfs.py
@@ -28,19 +28,23 @@ class DepthFirstSearch:
             # Remove a node from the frontier
             node = frontier.remove()
 
-            # Mark the node as explored
-            explored[node.state] = True
-
             # Return if the node contains a goal state
             if node.state == grid.end:
                 return Solution(node, explored)
 
-            # BFS
-            neighbours = grid.get_neighbours(node.state)
-            for action, postion in neighbours.items():
-                if postion not in explored:
-                    new_node = Node(
-                        "", postion, node.cost + grid.get_cost(postion), parent=node, action=action)
-                    frontier.add(new_node)
+            if node.state not in explored:
+                # Mark the node as explored
+                explored[node.state] = True
+
+                neighbours = grid.get_neighbours(node.state)
+                for action, postion in neighbours.items():
+                    if postion not in explored:
+                        child_node = Node(
+                            value="",
+                            state=postion,
+                            cost=node.cost + grid.get_cost(postion),
+                            parent=node,
+                            action=action)
+                        frontier.add(child_node)
 
         return NoSolution(explored)
