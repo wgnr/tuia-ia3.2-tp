fbf1d64031f86edca6ef5fa5ad54095789da20af
Juan Wagner <juanswagner@gmail.com>
2023-05-03 14:42:29 -0300

    add ucs

--- src/pathfinder/search/ucs.py
+++ src/pathfinder/search/ucs.py
@@ -1,5 +1,5 @@
 from ..models.grid import Grid
-from ..models.frontier import StackFrontier
+from ..models.frontier import PriorityQueueFrontier
 from ..models.solution import NoSolution, Solution
 from ..models.node import Node
 
@@ -16,12 +16,33 @@ class UniformCostSearch:
             Solution: Solution found
         """
         # Initialize a node with the initial position
-        node = Node("", grid.start, 0)
+        node = Node(value="root", state=grid.start, cost=0)
 
         # Initialize the explored dictionary to be empty
-        explored = {} 
-        
-        # Add the node to the explored dictionary
-        explored[node.state] = True
-        
+        explored: dict[tuple[int, int], Node] = {}
+
+        frontier = PriorityQueueFrontier()
+        frontier.add(node, node.cost)
+
+        while not frontier.is_empty():
+            node = frontier.pop()
+
+            if node.state == grid.end:
+                return Solution(node, explored)
+
+            neighbours = grid.get_neighbours(node.state)
+
+            for action, postion in neighbours.items():
+                child_cost = node.cost + grid.get_cost(postion)
+
+                if postion not in explored or child_cost < explored[postion].cost:
+                    child_node = Node(
+                        value="",
+                        state=postion,
+                        cost=child_cost,
+                        parent=node,
+                        action=action)
+                    frontier.add(child_node, child_node.cost)
+                    explored[child_node.state] = child_node
+
         return NoSolution(explored)
